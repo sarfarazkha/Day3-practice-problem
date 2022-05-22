b36384334429cf1dfff589ea971f340225a1cfa7
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-05-22 22:43:17 +0530

    UC3 No play

---
+++ src/com/bridgelabz/SnakeLadder/SnakeLadderUC3.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.SnakeLadder;
+import java.util.Random;
+
+
+public class SnakeLadderUC3 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int dieNum = random.nextInt(6) + 1;
+        int options = random.nextInt(4);
+        int position = 0;
+        final int ladder = 1;
+        final int noPlay = 2;
+        final int snake = 3;
+        switch (options) {
+            case ladder:
+                position += dieNum;
+                System.out.println("the player moved by the ladder by:" + position);
+                break;
+            case noPlay:
+                System.out.println("the player remains in the same position:" + position);
+                break;
+            case snake:
+                position -= dieNum;
+                System.out.println("the snake had bite the player by:" + position);
+                break;
+
+        }
+        System.out.println("position is:" + position);
+    }
+}
