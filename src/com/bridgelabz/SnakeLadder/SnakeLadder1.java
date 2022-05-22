7fb5d7f9b755e68512e162e031d62105e048ca95
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-05-22 22:21:06 +0530

    UC1 starting with 0

---
+++ src/com/bridgelabz/SnakeLadder/SnakeLadder1.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.SnakeLadder;
+
+public class SnakeLadder1 {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("the initial position of player one:" + position);
+    }
+}
