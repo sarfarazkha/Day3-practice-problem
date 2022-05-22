31defda720158ccc5cd28eeaefbaa0b6e0d47aa6
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-05-22 22:47:00 +0530

    UC4 win position 100

--- src/com/bridgelabz/SnakeLadder/SnakeLadder4.java
+++ src/com/bridgelabz/SnakeLadder/SnakeLadder4.java
@@ -1,2 +1,34 @@
-package com.bridgelabz.SnakeLadder;public class SnakeLadder4 {
+package com.bridgelabz.SnakeLadder;
+
+import java.util.Random;
+
+public class SnakeLadder4 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int position = 0;
+        final int ladder = 1;
+        final int noPlay = 2;
+        final int snake = 3;
+        while (position < 100) {
+            int dieNum = random.nextInt(6) + 1;
+            int options = random.nextInt(4);
+
+            switch (options) {
+                case ladder:
+                    if (position + dieNum > 100)
+                        break;
+                    position += dieNum;
+                    break;
+                case noPlay:
+                    break;
+                case snake:
+                    position -= dieNum;
+                    position = Math.max(position, 0);
+                    break;
+
+            }
+            System.out.println("position is:" + position);
+        }
+    }
 }
